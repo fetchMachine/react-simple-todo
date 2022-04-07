dff37c44bc2b692b74e43fc79e4748ace0ca2830
Roman Makretski <32076718+fetchMachine@users.noreply.github.com>
2022-04-07 17:49:22 +0300

    fix: fixed CheckboxGroup

--- src/components/common/Checkbox.jsx
+++ src/components/common/Checkbox.jsx
@@ -1,17 +1,13 @@
-function Checkbox({ label, value, checked }) {
+function Checkbox({ label, value, checked, onChange }) {
   return (
     <label>
-      <input type="radio" name="users" value={value} checked={checked}  />
+      <input type="radio" name="users" value={value} checked={checked} onChange={onChange}  />
       {label}
     </label>
   );
 }
 
-export const CheckboxGroup = ({ value: groupValue, options }) => {
-  return (
-    <>
-      {options.map(({ value, label }) =>
-          <Checkbox value={value} label={label} checked={value === groupValue} />)}
-    </>
-  )
+export const CheckboxGroup = ({ value: groupValue, options, onChange }) => {
+  return options.map(({ value, label }) =>
+          <Checkbox value={value} label={label} checked={value === groupValue} onChange={onChange} />)
 }
